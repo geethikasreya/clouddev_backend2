543fafab17fa5077d0f203d5a2f0b5f8929f7642
Geethika sreya <135309107+geethikasreya@users.noreply.github.com>
2025-04-17 22:12:51 -0500

    Added index.js

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ app.use(express.json());
 // MySQL connection config
 // triggering azure deployment
 const db = mysql.createConnection({
-  host: 'geethikasreya.mysql.database.azure.com',
-  user: 'mysql_admin',
-  password: 'Test@123',
+  host: 'cloudhw2.mysql.database.azure.com',
+  user: 'shreya',
+  password: 'Shreya@123',
   database: 'studentdb',
   port: 3306,
   connectTimeout:10000,
@@ -32,5 +32,5 @@ app.get('/api/students', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => console.log(`Backend running on http://localhost:${PORT}`));
